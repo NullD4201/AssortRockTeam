722d6c44e91ff6bd000a2d0f42ec650acbfd515e
DESKTOP-COA1C72\AR student <test@gmail.com>
2024-03-22 10:58:20 +0900

    2403222 민재홍 PlayerAnimInstance 이동속도,방향 등록

--- KDT1/Source/KDT1/Character/PlayerAnimInstance.cpp
+++ KDT1/Source/KDT1/Character/PlayerAnimInstance.cpp
@@ -2,4 +2,36 @@
 
 
 #include "PlayerAnimInstance.h"
+#include "MainCharacter.h"
+
+void UPlayerAnimInstance::NativeInitializeAnimation()
+{
+	Super::NativeInitializeAnimation();
+}
+
+// 이 함수는 매 프레임마다 동작한다.
+void UPlayerAnimInstance::NativeUpdateAnimation(float DeltaSeconds)
+{
+	Super::NativeUpdateAnimation(DeltaSeconds);
+
+	// TryGetPawnOwner() : 이 애니메이션 블루프린트를 사용하고 있는 Pawn 객체를 얻어온다.
+	// Cast를 이용하여 AMainCharacter 클래스로 형변환하여 받아온다.
+	AMainCharacter* OwnerPawn = Cast<AMainCharacter>(TryGetPawnOwner());
+
+	// IsValid : 객체가 유효한지 체크한다.
+	if (IsValid(OwnerPawn))
+	{
+		UCharacterMovementComponent* Movement = OwnerPawn->GetCharacterMovement();
+
+		if (IsValid(Movement))
+		{
+			// 속도 벡터의 크기를 구해서 이동속도로 사용한다.
+			mMoveSpeed = Movement->Velocity.Length();
+
+			// 속도의 비율을 구한다.
+			mMoveSpeed /= Movement->MaxWalkSpeed;
+		}
+	}
+}
+
 
--- KDT1/Source/KDT1/Character/PlayerAnimInstance.h
+++ KDT1/Source/KDT1/Character/PlayerAnimInstance.h
@@ -7,12 +7,21 @@
 #include "PlayerAnimInstance.generated.h"
 
 /**
- * 
+ *
  */
 UCLASS()
 class KDT1_API UPlayerAnimInstance : public UAnimInstance
 {
 	GENERATED_BODY()
-	
 
+protected:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	float	mMoveDir;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	float	mMoveSpeed;
+
+public:
+	virtual void NativeInitializeAnimation();
+	virtual void NativeUpdateAnimation(float DeltaSeconds);
 };
