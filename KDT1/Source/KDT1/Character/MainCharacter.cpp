579649a63943fdc8e0bff04fb67d588337eefb60
DESKTOP-COA1C72\AR student <test@gmail.com>
2024-04-11 15:06:43 +0900

    no message

--- KDT1/Source/KDT1/Character/MainCharacter.cpp
+++ KDT1/Source/KDT1/Character/MainCharacter.cpp
@@ -9,7 +9,7 @@
 // Sets default values
 AMainCharacter::AMainCharacter()
 {
- 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
 	mIdleMaxSpeed = 600.f;
@@ -56,7 +56,6 @@ void AMainCharacter::Tick(float DeltaTime)
 
 	PlayerTargetLocked(DeltaTime);
 
-	//GEngine->AddOnScreenDebugMessage(-1, 1.f, FColor::Green, FString::SanitizeFloat(mTargetArray.Num()));
 }
 
 // Called to bind functionality to input
@@ -82,7 +81,6 @@ void AMainCharacter::PlaySprint()
 	{
 		return;
 	}
-	// 변화 없음
 
 	mIsSprinting = true;
 	mSpeedTime = 0.f;
@@ -127,7 +125,7 @@ void AMainCharacter::PlayerWalkSpeedUpSmoothly(float DeltaTime)
 		if (mSpeedTime >= mDuration)
 		{
 			mSpeedTime = 0.f;
-			mCurrentMaxWalkSpeed += 50.f;
+			mCurrentMaxWalkSpeed += 100.f;
 		}
 	}
 	if (mIsSprinting == false && mCurrentMaxWalkSpeed > mIdleMaxSpeed)
@@ -137,7 +135,7 @@ void AMainCharacter::PlayerWalkSpeedUpSmoothly(float DeltaTime)
 		if (mSpeedTime >= mDuration)
 		{
 			mSpeedTime = 0.f;
-			mCurrentMaxWalkSpeed -= 50.f;
+			mCurrentMaxWalkSpeed -= 100.f;
 		}
 	}
 	GetCharacterMovement()->MaxWalkSpeed = mCurrentMaxWalkSpeed;
@@ -155,7 +153,7 @@ void AMainCharacter::TargetLock()
 		return;
 	}
 
-	if (mAnimInst->GetAnimType() == EPlayerAnimType::Dodge || mAnimInst->GetAnimType() == EPlayerAnimType::Attack 
+	if (mAnimInst->GetAnimType() == EPlayerAnimType::Dodge || mAnimInst->GetAnimType() == EPlayerAnimType::Attack
 		|| mAnimInst->GetAnimType() == EPlayerAnimType::Skill)
 		return;
 
@@ -172,7 +170,6 @@ void AMainCharacter::TargetLock()
 
 #if ENABLE_DRAW_DEBUG
 
-	// 구를 그린다.
 	FColor	DrawColor = IsCollision ? FColor::Red : FColor::Green;
 
 	DrawDebugCapsule(GetWorld(), (StartLocation + EndLocation) / 2.f,
@@ -197,18 +194,12 @@ void AMainCharacter::PlayerTargetLocked(float DeltaTime)
 		FVector  TargetVec = TargetActor->GetActorLocation();
 		FVector  ActorVec = GetActorLocation();
 
-		FRotator ViewRot  = GetControlRotation();
+		FRotator ViewRot = GetControlRotation();
 
 		FRotator PlayerRot = FRotationMatrix::MakeFromX(TargetVec - ActorVec).Rotator();
-		//FRotator PlayerFindRot = UKismetMathLibrary::FindLookAtRotation(ActorVec, TargetVec);
-		//(MakeFromX 랑 같은 함수)
-
-		//PlayerRot.Pitch = GetController()->K2_GetActorRotation().Pitch;
-		//(TargetLock 된 상태에서 마우스 Pitch 값만 사용하는 기능 /
-		// MainController에서 Input 안들어오게 설정해놔서 지금은 의미없음)
 
 		FRotator RInter = FMath::RInterpTo(ViewRot, PlayerRot, DeltaTime, 5.f);
-		
+
 		GetController()->SetControlRotation(RInter);
 
 		CheckPlayerCameraAngle();
@@ -253,19 +244,18 @@ void AMainCharacter::CheckPlayerCameraAngle()
 
 void AMainCharacter::SwitchLeft()
 {
-	// 지금 Target Lock 된 Actor 가 아닐 경우 실행
-	// Closest Right AI 변수 하나 만들어놓고 한번 초기화 시켜주고
-	// 반복문을 돌면서 , (mTargetArray .Num 만큼)
-	// Target이랑 Array AI 랑 Actor Right Vec 이랑 내적 값 잡아서
-	// RIght 가져오고, 그 중에서 값 비교해서 가장 오른쪽 AI 변수에 넣어주고
-	// Target 변경 해주기
-
 	if (!mIsTargetLocked)
 		return;
 
+	if (mAnimInst->GetAnimType() == EPlayerAnimType::Dodge || mAnimInst->GetAnimType() == EPlayerAnimType::Attack
+		|| mAnimInst->GetAnimType() == EPlayerAnimType::Skill || mAnimInst->GetAnimType() == EPlayerAnimType::CoolDown)
+	{
+		return;
+	}
+
 	ClosestLeftAI = nullptr;
 	mLeftAIArray.Reset();
-	
+
 	for (int32 i = 0; i < mTargetArray.Num(); ++i)
 	{
 		if (mTargetArray[i] != TargetActor)
@@ -287,7 +277,6 @@ void AMainCharacter::SwitchLeft()
 	if (mLeftAIArray.Num() > 0)
 		ClosestLeftAI = mLeftAIArray[0];
 
-	// LeftArray 배열 중에서 가장 Target과 가까운 놈 찾아내기
 	for (int32 i = 0; i < mLeftAIArray.Num(); ++i)
 	{
 		float NewAIDistance = FVector::Distance(TargetActor->GetActorLocation(), mLeftAIArray[i]->GetActorLocation());
@@ -309,6 +298,12 @@ void AMainCharacter::SwitchRight()
 {
 	if (!mIsTargetLocked)
 		return;
+
+	if (mAnimInst->GetAnimType() == EPlayerAnimType::Dodge || mAnimInst->GetAnimType() == EPlayerAnimType::Attack
+		|| mAnimInst->GetAnimType() == EPlayerAnimType::Skill || mAnimInst->GetAnimType() == EPlayerAnimType::CoolDown)
+	{
+		return;
+	}
 
 	ClosestRightAI = nullptr;
 	mRightAIArray.Reset();
@@ -333,7 +328,6 @@ void AMainCharacter::SwitchRight()
 	if (mRightAIArray.Num() > 0)
 		ClosestRightAI = mRightAIArray[0];
 
-	// LeftArray 배열 중에서 가장 Target과 가까운 놈 찾아내기
 	for (int32 i = 0; i < mRightAIArray.Num(); ++i)
 	{
 		float NewAIDistance = FVector::Distance(TargetActor->GetActorLocation(), mRightAIArray[i]->GetActorLocation());
@@ -351,39 +345,9 @@ void AMainCharacter::SwitchRight()
 	}
 }
 
-void AMainCharacter::CheckDotValueInRadius(float DeltaTime)
-{
-	FCollisionQueryParams	param(NAME_None, false, this);
-
-	FVector StartLocation = GetActorLocation();
-	FVector EndLocation = GetActorLocation() + 1.f;
-
-	FHitResult	result;
-
-	bool IsCollision = GetWorld()->SweepSingleByChannel(result, StartLocation, EndLocation,
-		FQuat::Identity, ECC_GameTraceChannel2, FCollisionShape::MakeSphere(1000.f),
-		param);
-
-	if (IsCollision)
-	{
-		FVector CameraForwardVec = mCamera->GetForwardVector();
-		FVector ToTargetVec = result.GetActor()->GetActorLocation() - GetActorLocation();
-		ToTargetVec.Normalize();
-
-		FVector TargetVec = ToTargetVec - GetActorForwardVector();
-		TargetVec.Normalize();
-
-		float DotValue = FVector::DotProduct(CameraForwardVec, TargetVec);
-
-		GEngine->AddOnScreenDebugMessage(-1, 1.f, FColor::Blue, FString::SanitizeFloat(DotValue));
-
-	}
-}
-
 void AMainCharacter::OnActiveRadiusBeginOverlap(UPrimitiveComponent* OverlappedComp, AActor* Other, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
 	mTargetArray.Add(Other);
-
 }
 
 void AMainCharacter::OnActiveRadiusEndOverlap(UPrimitiveComponent* OverlappedComp, AActor* Other, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
